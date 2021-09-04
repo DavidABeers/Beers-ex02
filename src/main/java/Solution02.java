7c53613c8a8c0207fa39d10c005324af8e860b0f
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-09-04 18:59:08 -0400

    Wrote copyright and pseudocode above class declaration and executed the solution presented in the pseudocode.

--- src/main/java/Solution02.java
+++ src/main/java/Solution02.java
@@ -1,5 +1,23 @@
-public class Solution02 {
-    public static void main(String []args){
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 David Beers
+ */
 
+/*
+ *   print a prompt for input string
+ *   store the input string
+ *   get the number of characters within the stored string
+ *   print the number of characters in the string
+ */
+
+import java.util.Scanner;
+
+public class Solution02 {
+
+    public static void main(String []args){
+        System.out.println("What is the string to count characters for?");
+        Scanner inputScanner  = new Scanner(System.in);
+        String inputString = inputScanner.nextLine();
+        System.out.printf("%s has %d characters", inputString, inputString.length());
     }
 }
